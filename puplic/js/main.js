537270af6ec39be1a782ef1147d78018ed2ae014
mahmod mostafa <94807913+mransho@users.noreply.github.com>
2024-03-01 05:10:09 +0200

    Update main.js

--- puplic/js/main.js
+++ puplic/js/main.js
@@ -1,23 +1,23 @@
-if (localStorage.getItem("isSmall") === "yes") {
-  SidebarID.classList.add("small-sidebare");
+if (localStorage.getItem("isbig") === "yes") {
+  SidebarID.classList.add("big-sidebare");
 } else {
-  SidebarID.classList.remove("small-sidebare");
+  SidebarID.classList.remove("big-sidebare");
 }
 const togglesidebar = () => {
-  if (localStorage.getItem("isSmall") === "yes") {
-    localStorage.setItem("isSmall", "no");
-    SidebarID.classList.remove("small-sidebare");
+  if (localStorage.getItem("isbig") === "yes") {
+    localStorage.setItem("isbig", "no");
+    SidebarID.classList.remove("big-sidebare");
   } else {
-    localStorage.setItem("isSmall", "yes");
-    SidebarID.classList.add("small-sidebare");
+    localStorage.setItem("isbig", "yes");
+    SidebarID.classList.add("big-sidebare");
   }
 };
 
 window.addEventListener("load", function () {
   var currentScreenWidth = window.innerWidth;
   if (currentScreenWidth < 400) {
-    localStorage.setItem("isSmall", "yes");
-    SidebarID.classList.add("small-sidebare");
+    localStorage.setItem("isbig", "no");
+    SidebarID.classList.remove("big-sidebare");
   } else {
   }
 });
